696ae2a3277b067af5a6134118cad50d1de154da
David <david.costa.car@gmail.com>
2025-01-17 00:01:25 +0100

    All good but /tmp/aesd-autograder/Image missing

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,14 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+	# START DC_MODS
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper #clean removing .config file
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig #Setup default config
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all #Build kernel image
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs #Build devicetree
+    # DONE LATER? make make -C "$FINDER_APP_DIR" CROSS_COMPILE=aarch64-none-linux-gnu-
+    #qemu-system-arm -m 256M -nographic -M versatilepb -kernel zImage -append "console=ttyAMA0 rdinit=bin/sh" -dtb versatile-pb.dtb -initrd initramfs.cpio.gz
+	# END DC_MODS
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +56,14 @@ then
 fi
 
 # TODO: Create necessary base directories
+# DC_ROOM:
+mkdir -p rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+cd ..
+#END DC_ROOM
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +72,93 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    #make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} menuconfig
+	make defconfig
 else
     cd busybox
 fi
 
+# DC_ROOM:
 # TODO: Make and install busybox
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd "$OUTDIR"
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+INTERPRETER=$(${CROSS_COMPILE}readelf -a rootfs/bin/busybox | \
+              grep "Requesting program interpreter:" | \
+              awk -F': ' '{print $2}' | sed 's/]*$//')
+SHAREDLIB=$(${CROSS_COMPILE}readelf -a rootfs/bin/busybox | \
+            grep "Shared library:" | \
+            sed -e 's/.*Shared library: *\[//' -e 's/\].*$//')
+
+SYSROOT=$(${CROSS_COMPILE}gcc --print-sysroot)
+
+if [ -z "$SYSROOT" ]; then
+    echo "Error: Could not determine sysroot path."
+    exit 1
+fi
+
+echo "Using sysroot: $SYSROOT" #TODO: DELETE THIS
+
+# Copy the interpreter to rootfs
+if [ ! -z "$INTERPRETER" ]; then
+    #INTERPRETER_PATH=$(dirname "$INTERPRETER")
+    INTERPRETER_PATH=$(find "$SYSROOT" -name $(basename "$INTERPRETER"))
+    mkdir -p "rootfs${INTERPRETER_PATH}"
+    #cp -v $(find ${OUTDIR}/rootfs -name $(basename "$INTERPRETER")) "rootfs${INTERPRETER}"
+    cp -v "$INTERPRETER_PATH" "rootfs${INTERPRETER}"
+fi
+
+# Copy the shared libraries to rootfs
+for LIB in $SHAREDLIB; do
+    LIB_PATH=$(find "$SYSROOT" -name "$LIB")
+    if [ ! -z "$LIB_PATH" ]; then
+        DEST_DIR="rootfs$(dirname "$LIB_PATH")"
+        mkdir -p "$DEST_DIR"
+        cp -v "$LIB_PATH" "$DEST_DIR"
+    else
+        echo "Warning: Library $LIB not found in sysroot."
+    fi
+done
 
 # TODO: Make device nodes
 
+mkdir -p rootfs/dev
+
+sudo mknod -m 666 rootfs/dev/null c 1 3
+sudo mknod -m 600 rootfs/dev/console c 5 1
+
+
 # TODO: Clean and build the writer utility
+make -C "$FINDER_APP_DIR" clean #-C tells make to change to the directory specified by $FINDER_APP_DIR
+make -C "$FINDER_APP_DIR" CROSS_COMPILE=aarch64-none-linux-gnu-
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/conf/username.txt ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/conf/assignment.txt ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home
+#cp ${FINDER_APP_DIR}/start-qemu-app.sh ${OUTDIR}/rootfs/home
+#cp ${FINDER_APP_DIR}/autorun-terminal.sh ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home
 
 # TODO: Chown the root directory
+sudo chown -R root rootfs
 
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f initramfs.cpio
+
+echo "############################ DC ROOM 4 #####################"
+
+#qemu-system-arm -m 256M -nographic -M versatilepb -kernel zImage -append "console=ttyAMA0 rdinit=bin/sh" -dtb versatile-pb.dtb -initrd initramfs.cpio.gz
+"$FINDER_APP_DIR/start-qemu-terminal.sh" "${OUTDIR}"
+"$FINDER_APP_DIR/start-qemu-app.sh" "${OUTDIR}"
+
+#END DC_ROOM
