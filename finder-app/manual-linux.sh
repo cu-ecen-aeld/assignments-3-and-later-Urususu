022c904e70fa1f21f8509be5e04ed85079193ff3
David <david.costa.car@gmail.com>
2025-01-31 09:59:22 +0100

    Solve issues with deleting a folder 3

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -154,7 +154,7 @@ sudo chown -R root rootfs
 # Task: Create initramfs.cpio.gz
 
 if [ -e ${OUTDIR}/initramfs.cpio.gz ]; then
-    rm ${OUTDIR}/initramfs.cpio.gz
+    rm -f ${OUTDIR}/initramfs.cpio.gz
 fi
 
 cd rootfs
