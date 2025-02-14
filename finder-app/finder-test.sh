c233043c53b048415d5c30965152e92cfb9a37be
David <david.costa.car@gmail.com>
2025-02-14 11:18:11 +0100

    Modification for assignment4-2 adaptation 3

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -60,7 +60,7 @@ do
 	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(.${CURRDIR}/finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(${CURRDIR}/finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
