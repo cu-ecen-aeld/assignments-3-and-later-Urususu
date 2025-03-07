f6c21905c0aea374b596cf157c7d0acc6b071875
David <david.costa.car@gmail.com>
2025-03-07 21:31:25 +0100

    Local test success

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -10,11 +10,13 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h> 
+#include <linux/fs.h>
 
 #define GOOD_VAL 0
 #define ERR_VAL -1
 
-#define RECV_BUFFER 100
+#define RECV_BUFFER 512
+#define SOCKET_BACKLOG 100
 
 /* Returns -1 if any of the steps setting the socket fails */
 
@@ -53,7 +55,7 @@ int socket_creation(int * sock_fd, struct sockaddr * client_addr)
 	}	
 	if(ret!=ERR_VAL){
 		//Listen listen() for new connections (Backlog to determine how many connections are allowed)
-		ret = listen(*sock_fd,1); //Accepts up to 1 connection?
+		ret = listen(*sock_fd,SOCKET_BACKLOG); //Accepts up to 1 connection?
 		if(ret<0){
 			perror("Listen error");
 		}		
@@ -84,19 +86,38 @@ int socket_connection(int sockfd, int * client_sock, struct sockaddr * client_ad
 int return_data_to_client(int file_fd, int sock_fd)
 {
 	int ret = GOOD_VAL;
-	char read_buff[256];
+	//char read_buff[256];
 	
-	ssize_t bytes_read;
+	char *send_string = malloc(RECV_BUFFER);
+	memset(send_string,'\0',RECV_BUFFER); //Add null everywhere
+	ssize_t current_size = RECV_BUFFER;
+	ssize_t bytes_read_total = 0;
+	ssize_t bytes_read = 0;
+	
 	//Read data from file
 	lseek(file_fd, 0, SEEK_SET); // Move to the beginning of the file
-
-	while((bytes_read = read(file_fd, read_buff, sizeof(read_buff))) > 0){ //0 indicates end of file
-		//Write data to client
-		int sent_bytes = send(sock_fd, read_buff, bytes_read, 0);
-		if(sent_bytes<0){
-			perror("Error sending data to client");
-			return sent_bytes;
-		}
+	while((bytes_read = read(file_fd, send_string + bytes_read_total, RECV_BUFFER)) > 0){ //0 indicates end of file
+		bytes_read_total += bytes_read;
+		if(bytes_read_total >= current_size){
+			ssize_t new_size = current_size + RECV_BUFFER;
+			char *aux_buffer = realloc(send_string, new_size);
+			if(!aux_buffer){
+				perror("Error reallocating read size");
+				free(send_string);
+				ret = ERR_VAL;
+				return ret;
+			}
+			current_size = new_size;
+			send_string = aux_buffer;
+		}	
+	}
+	//Once the file is completely read, send:
+	//printf("send: %s,\n\n bytes_read_total: %ld\n\n", send_string, bytes_read_total);
+	int sent_bytes = send(sock_fd, send_string, bytes_read_total, 0);
+	free(send_string);
+	if(sent_bytes<0){
+		perror("Error sending data to client");
+		return sent_bytes;
 	}
 	return ret;
 }
@@ -111,7 +132,7 @@ static void signal_handler(int signal_number)
 	}
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
 	int ret = GOOD_VAL;
 	int sockfd = 0;
@@ -138,6 +159,41 @@ int main(void)
 	//Socket creation and connection with client:
 	ret = socket_creation(&sockfd, client_addr);	
 
+	if(argc>1){
+		if(strcmp(argv[1], "-d") == GOOD_VAL){
+			//Start as daemon
+			pid_t pid = fork();
+			if(pid == 0){
+				//This is the child
+			}
+			else if(pid == -1){
+				return -1;
+			}
+			else{
+				exit(EXIT_SUCCESS);
+			}
+			
+			//Create new session and new process group for this daemon to be a leader
+			if(setsid() == -1){
+				return -1;
+			}
+			
+			//Set working directory to root
+			if(chdir("/") == -1){
+				return -1;
+			}
+			
+			//Close all open files
+			/*for(int i=0; i< sysconf(_SC_OPEN_MAX); i++){
+				close(i);
+			}*/
+			
+			//Redirect fd's 0,1,2 to /dev/null
+			open("/dev/null", O_RDWR); //stdin
+			dup(0);	//stdout
+			dup(0); //stderror			
+		}
+	}
 	//Loop until SIGINT or SIGTERM
 	while((aesdsock_close_gracefully == false)&&(ret!=ERR_VAL)){ //As long as SIGINT or SIGTERM are not set
 		if(ret == GOOD_VAL){
@@ -145,9 +201,11 @@ int main(void)
 		}
 		if(ret!=ERR_VAL){ //If connection is stablished properly
 			int outfile = open("/var/tmp/aesdsocketdata", O_CREAT | O_RDWR | O_APPEND, 0777);
+			//int outfile = open("/home/david/Documents/_David/_Coursera/aesdtest", O_CREAT | O_RDWR | O_APPEND, 0777);
 			if (outfile == -1){
 				perror("Error opening aesdsocketdata file");
-				return -1;
+				ret = ERR_VAL;
+				break;
 			}
 			//char recv_string[RECV_BUFFER];
 			char *string_buffer = malloc(RECV_BUFFER);
@@ -158,53 +216,56 @@ int main(void)
 			if(outfile != ERR_VAL){ //Create or open aesdsocketdata file
 				int bytes_recv = 0;
 				int total_bytes_recv = 0;
-					while((bytes_recv = recv(client_sock, string_buffer + total_bytes_recv, current_size - extra_size - 1, 0)) > 0){ //keep reading while there are bytes received
+				ssize_t written_bytes = 0;
+					while((bytes_recv = recv(client_sock, string_buffer + total_bytes_recv, RECV_BUFFER, 0)) > 0){ //keep reading while there are bytes received
 						total_bytes_recv += bytes_recv;
-						//printf("Received packet: %s\n Bytes recv: %d\n total_bytes_recv = %d\n stringsize = %ld\n", string_buffer, bytes_recv, total_bytes_recv, current_size ); //TODO: DELETE
 						                    
 						if(strchr(string_buffer, '\n')){ //If there is already a packet end
 							string_buffer[total_bytes_recv] = '\0'; //NULL terminate string
-							//printf("There is a newline found\n"); //TODO: DELETE
-							write(outfile, string_buffer, total_bytes_recv); //appends packet
+							written_bytes = write(outfile, string_buffer, total_bytes_recv); //appends packet
+							if(written_bytes<0){
+								perror("Error writing to file");
+							}
+							fsync(outfile);
 							extra_size = 0;					
 							memset(string_buffer,'\0',RECV_BUFFER); //Add null everywhere
 							//Return full content of /var/tmp/aesdsocketdata to client
 							return_data_to_client(outfile, client_sock);
-							printf("Done write and send\n");
 							total_bytes_recv = 0;
 						}
-						else if(bytes_recv >= (current_size-1)){
-							printf("Need to expand buffer\n"); //TODO: DELETE
+						else if(total_bytes_recv >= current_size){
 							extra_size = RECV_BUFFER;
 							char *expanded_buff = realloc(string_buffer, current_size + extra_size); //Expand buffer by the necessary size
 							current_size = current_size + extra_size;
 							if(expanded_buff == NULL){
-								perror("Reallocation failed");
+								perror("Reallocation failed for recv");
 								free(expanded_buff);
 								free(string_buffer);
 								close(outfile);
 								close(client_sock);
-								return -1;
+								ret = ERR_VAL;
+								break;
 							}
 
 							string_buffer = expanded_buff; //So now the string buffer is the new expanded_buff
 						}
 						else{
-							printf("No need to expand\n"); //TODO: Delete
 							extra_size = 0;
 						}				
 					}
 			}
 			free(string_buffer);
 			close(outfile); //Close file handler
-			remove("/var/tmp/aesdsocketdata");
 			syslog(LOG_INFO, "Closed connection from %s", client_addr->sa_data);
 		}
 		else{
-			return ret;
+			ret = ERR_VAL;
+			break;
 		}
 		close(client_sock);
 	}
+	remove("/var/tmp/aesdsocketdata");
+	close(client_sock);
 	close(sockfd);
 	
 	return ret;
